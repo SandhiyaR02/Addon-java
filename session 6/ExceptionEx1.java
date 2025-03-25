b6c9405eb122f9cb2638ece5ea75df5303858fb8
SandhiyaR02 <sandhiyabala2006@gmail.com>
2025-03-25 15:25:22 +0530

    session6

---
+++ session 6/ExceptionEx1.java
@@ -0,0 +1,22 @@
+
+package session6;
+
+public class ExceptionEx1 {
+	public static void main(String[] args) {
+		try {
+			System.out.println("Trying to divide by 0...");
+			int res = 10/0;
+			System.out.println("Result: "+res);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			
+		}finally {
+			System.out.println("finally block excecuted..");
+		}
+		
+	}
+
+	
+}
---
+++ session 6/ExceptionEx2.java
@@ -0,0 +1,19 @@
+package session6;
+
+public class ExceptionEx2 {
+	static void validateAge(int age) {
+		if(age<18) {
+			throw new IllegalArgumentException("Age must be 18 or above");
+		}else {
+			System.out.println("Age is valid");
+		}
+	}
+public static void main(String[] args) {
+	try {
+		validateAge(15);
+	}catch(IllegalArgumentException e){
+		System.out.println("Caught an exception: "+e.getMessage());
+		
+	}
+}
+}
